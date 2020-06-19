3d8b263b481825125a103cee23c55c0d4d77c18b
Jessica Aguirre <jessica.g.aguirre@gmail.com>
2020-06-18 20:32:42 -0500

    Updates

---
+++ precache-manifest.4c958583ce1d009737f2ef1a1e87b9d5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8225d7b5de3efe2c6bbe0a5ae9cfbe0a",
+    "url": "/JessicaAguirre25/carritodecompras.git/index.html"
+  },
+  {
+    "revision": "8f8f945a3fbbabc63ad0",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/css/2.eb5ad911.chunk.css"
+  },
+  {
+    "revision": "1b293fb168497279376a",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f8f945a3fbbabc63ad0",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/js/2.e0c8f009.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/js/2.e0c8f009.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b293fb168497279376a",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/js/main.58da3e30.chunk.js"
+  },
+  {
+    "revision": "56397783cb6dea15accf",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/js/runtime-main.91b390d3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/JessicaAguirre25/carritodecompras.git/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
